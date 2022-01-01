d06950f73b86d36f903d452315f95aff47aafadf
Ben Chatelain <ben@octop.ad>
2021-12-31 21:20:37 -0700

    ☠️ Add template library package

---
+++ Tests/SwiftBinaryPackageTests/SwiftBinaryPackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftBinaryPackage
+
+final class SwiftBinaryPackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftBinaryPackage().text, "Hello, World!")
+    }
+}
